0656f4dba5e0d6ace9878a250f722a9a0c29f2a3
DRJungOok <DRJungOok@gmail.com>
2024-11-24 15:55:39 +0900

    create Project ZeroToDeploy

---
+++ src/main/java/com/jungook/zerotodeploy/ZeroToDeployApplication.java
@@ -0,0 +1,13 @@
+package com.jungook.zerotodeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroToDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroToDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jungook/zerotodeploy/ZeroToDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jungook.zerotodeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroToDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
