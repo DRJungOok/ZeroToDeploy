2d8562c2a3ea3569ce4746eb9e15dc04d52418e6
DRJungOok <DRJungOok@gmail.com>
2025-01-26 21:34:21 +0900

    불필요 파일 삭제

---
+++ src/main/java/com/jungook/zerotodeploy/ServletConfig.java
@@ -0,0 +1,14 @@
+package com.jungook.zerotodeploy;
+
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServletConfig implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+
+	@Override
+	public void customize(ConfigurableServletWebServerFactory factory) {
+		factory.setContextPath("/zerotodeploy");
+	}
+}
