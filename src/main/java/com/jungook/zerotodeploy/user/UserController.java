06e3b383800dc4ae58e2602745b1c41663ea5b56
DRJungOok <DRJungOok@gmail.com>
2025-03-25 23:31:33 +0900

    edit

--- src/main/java/com/jungook/zerotodeploy/user/UserController.java
+++ src/main/java/com/jungook/zerotodeploy/user/UserController.java
@@ -1,34 +1,39 @@
 package com.jungook.zerotodeploy.user;
 
-import ch.qos.logback.core.model.Model;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
+
 	private final UserService userService;
+
 	@GetMapping("/signup")
-	public String signup(UserCreateForm form) {
+	public String signup(Model model) {
+		model.addAttribute("form", new UserCreateForm());
 		return "signUp";
 	}
+
 	@PostMapping("/signup")
-	public String signup(@Valid UserCreateForm form, BindingResult bindingResult) {
+	public String signup(@Valid UserCreateForm form, BindingResult bindingResult, Model model) {
 		if (bindingResult.hasErrors()) {
+			model.addAttribute("form", form);
 			return "signUp";
 		}
 
-		if(!form.getPassword1().equals(form.getPassword2())) {
-			bindingResult.rejectValue("password2", "Passwords do not match");
+		if (!form.getPassword1().equals(form.getPassword2())) {
+			bindingResult.rejectValue("password2", "비밀번호가 일치하지 않습니다.");
+			model.addAttribute("form", form);
 			return "signUp";
 		}
 
 		userService.create(form.getUsername(), form.getEmail(), form.getPassword1());
-		return "redirect:signup";
+		return "redirect:/signup";
 	}
 }
