41bcad11bc578cd4d14451f30fe0e647e8bbdd7d
DRJungOok <DRJungOok@gmail.com>
2025-05-07 17:03:13 +0900

    edit

--- src/main/java/com/jungook/zerotodeploy/ZeroToDeployApplication.java
+++ src/main/java/com/jungook/zerotodeploy/ZeroToDeployApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan("com.jungook.zerotodeploy")
+@SpringBootApplication
 public class ZeroToDeployApplication extends SpringBootServletInitializer{
 
 	@Override
