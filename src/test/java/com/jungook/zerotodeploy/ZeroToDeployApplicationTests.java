5593fcc3ecf25e6561606b6771b3cdfed8c691a8
DRJungOok <jeju8265@gmail.com>
2025-05-21 10:04:23 +0900

    빌드 안 되던거 수정

--- src/test/java/com/jungook/zerotodeploy/ZeroToDeployApplicationTests.java
+++ src/test/java/com/jungook/zerotodeploy/ZeroToDeployApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = ZeroToDeployApplication.class)
+@ActiveProfiles("test")
 class ZeroToDeployApplicationTests {
 
 	@Test
