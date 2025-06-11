f67656aeb40f8b8dc36020994408894a12baa979
DRJungOok <jeju8265@gmail.com>
2025-06-11 23:01:19 +0900

    edit friends package

--- src/main/java/com/jungook/zerotodeploy/friends/FriendsController.java
+++ src/main/java/com/jungook/zerotodeploy/friends/FriendsController.java
@@ -1,15 +1,33 @@
 package com.jungook.zerotodeploy.friends;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/friends")
+@RequiredArgsConstructor
 public class FriendsController {
-    @GetMapping("/friends")
+
+    private final FriendsService friendsService;
+
+    @GetMapping
     public String friends(Model model) {
         return "friends";
+    }
+
+    @PostMapping("/request/{username}")
+    public String sendRuest(@PathVariable String username, Authentication authentication) {
+        String sender = authentication.getName();
+        friendsService.sendFriendRequest(sender, username);
+        return "redirect:/friends";
+    }
+
+    @PostMapping("/accept/{id}")
+    public String acceptFriend(@PathVariable Long id) {
+        friendsService.acceptRequest(id);
+        return "redirect:/friends";
     }
 }
