7ab9cf3239f88572567d1f100044577ce812291d
DRJungOok <jeju8265@gmail.com>
2025-07-22 22:11:11 +0900

    new chat func

--- src/main/java/com/jungook/zerotodeploy/IndexController.java
+++ src/main/java/com/jungook/zerotodeploy/IndexController.java
@@ -9,4 +9,9 @@ public class IndexController {
     public String index() {
         return "index";
     }
+    
+    @GetMapping("/chatRoom")
+    public String chatRoom() {
+        return "chatRoom";
+    }
 }
