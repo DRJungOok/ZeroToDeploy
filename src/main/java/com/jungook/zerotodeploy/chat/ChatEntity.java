cef3d3e7599f525c91b14e634327d6ace23cc3da
DRJungOok <jeju8265@gmail.com>
2025-07-24 23:28:42 +0900

    edit chatEntity.java

--- src/main/java/com/jungook/zerotodeploy/chat/ChatEntity.java
+++ src/main/java/com/jungook/zerotodeploy/chat/ChatEntity.java
@@ -1,10 +1,11 @@
 package com.jungook.zerotodeploy.chat;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.jungook.zerotodeploy.joinMember.JoinUserEntity;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -16,9 +17,29 @@ public class ChatEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true)
     private String roomKey;
 
     @Column(unique = true, nullable = false)
     private String roomName;
+
+    @ManyToMany
+    @JoinTable(name = "chat_room_users", joinColumns = @JoinColumn(name = "chat_room_id"), inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private Set<JoinUserEntity> participants = new HashSet<>();
+
+    public static ChatEntity individual(String roomKey, JoinUserEntity user1, JoinUserEntity user2) {
+        Set<JoinUserEntity> users = new HashSet<>();
+        users.add(user1);
+        users.add(user2);
+        return ChatEntity.builder().roomKey(roomKey).
+         roomName(user1.getUserName() + " & " + user2.getUserName())
+        .participants(users).build();
+    }
+
+    public static ChatEntity several(String roomName, Set<JoinUserEntity> users) {
+        return ChatEntity.builder()
+                .roomName(roomName)
+                .participants(users)
+                .build();
+    }
 }
